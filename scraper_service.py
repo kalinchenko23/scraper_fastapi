10ebb90dd139788d6dcdd97fa23492cf0a294aea
Max <kalinchenko.max@gmail.com>
2023-11-05 10:18:40 +0100

    Hid the telephone number

--- scraper_service.py
+++ scraper_service.py
@@ -64,7 +64,7 @@ async def retrieve_message(client,channel, hours):
 
 async def retrieve_comments(client, channel, message_id):
     await client.connect()
-    if not await client.is_user_authorized():
+    if not await client.is_user_authorized():ls
         await client.send_code_request(phone_number)
         await client.sign_in(phone_number, input('Enter the code: '))
     try:
