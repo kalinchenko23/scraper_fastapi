694f13666504e3d5ed90c3d37eb37816618baee5
Maksym Kalinchenko <kalinchenko.max@gmail.com>
2024-04-18 21:22:02 -0400

    packaged app into docker container

--- sessions.py
+++ sessions.py
@@ -1,13 +1,11 @@
 from telethon import TelegramClient
-import json
 import os
 
-with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "secrets.json"), "r") as secrets:
-    data = json.load(secrets)
-    api_id, api_hash, bot_token, lang_detect_key,phone_number = data["api_info"]["api_id"], data["api_info"]["api_hash"], \
-                                                   data["api_info"]["bot_token"], data["lang_detect_info"]["api_key"], data["phone_number"]
+name = os.environ.get('NAME')
+api_id = os.environ.get('API_ID')
+api_hash = os.environ.get('API_HASH')
 
-client_max = TelegramClient('max', api_id, api_hash)
+client_max = TelegramClient(name, api_id, api_hash)
 
 
 
